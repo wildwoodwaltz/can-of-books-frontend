a43961a4532c77f4c7f2509bb2e6a2c81e6892d3
Eden Brekke <eden.lorrai@gmail.com>
2022-03-29 17:22:51 -0700

    it submit! it squee! it squoo! Modal heck ye

--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -4,38 +4,39 @@ import { Form, Container, Button } from 'react-bootstrap';
 
 class BookFormModal extends React.Component {
 
-  render() {
-    let handleBookShelf = (e) => {
-      e.preventDefault();
-      let newBook = {
-        title: e.target.title.value,
-        description: e.target.description.value,
-        email: e.target.email.value,
-        status: e.target.status.checked
-      }
-      this.props.addBook(newBook);
+  handleBookShelf = (e) => {
+    e.preventDefault();
+    let newBook = {
+      title: e.target.title.value,
+      description: e.target.description.value,
+      email: e.target.email.value,
+      status: e.target.status.checked
     }
+    this.props.addBook(newBook);
+    this.props.onHide();
+  }
+  render() {
 
     return (
       <Container className="mt-5">
-      <Form onSumbit={handleBookShelf}>
-        <Form.Group controlID="title">
-          <Form.Label>Title</Form.Label>
-          <Form.Control type="text"/>
-        </Form.Group>
-        <Form.Group controlID="description">
-          <Form.Label>Description</Form.Label>
-          <Form.Control type="text"/>
-        </Form.Group>
-        <Form.Group controlID="email">
-          <Form.Label>Title</Form.Label>
-          <Form.Control type="email"/>
-        </Form.Group>
-        <Form.Group controlId="Status">
-                <Form.Check type="checkbox" label="I have read this book" />
-              </Form.Group>
-              <Button type="submit">Add Book to my Shelf</Button>
-      </Form>
+        <Form onSubmit={this.handleBookShelf}>
+          <Form.Group controlId="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group controlId="description">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+          <Form.Group controlId="status">
+            <Form.Check type="checkbox" label="I have read this book" />
+          </Form.Group>
+          <Button type="submit" >Add Book to my Shelf</Button>
+        </Form>
       </Container>
     );
   }
